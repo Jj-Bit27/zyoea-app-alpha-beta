31213c59f17c3971744ea74fe9ad1cc09d4d734a
Juan Jose <juanglezjj070927@gmail.com>
2026-05-25 11:57:34 -0600

    arreglando cositas

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = import.meta.env.PUBLIC_API_URL || "http://localhost:8080";
+export const API_URL = import.meta.env.PUBLIC_API_URL || "https://api.suavus.app";
 export const GRAPHQL_URL = `${API_URL}/query`;
-export const FRONTEND_URL = import.meta.env.PUBLIC_FRONTEND_URL || "http://localhost:4321";
+export const FRONTEND_URL = import.meta.env.PUBLIC_FRONTEND_URL || "http://suavus.app";
