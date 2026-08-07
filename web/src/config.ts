ac3771dbfecdf99b7f454a797141c7bad25ec29b
Juan Jose <168502269+Jj-Bit27@users.noreply.github.com>
2026-08-07 09:45:19 -0600

    Update API_URL to new end

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.PUBLIC_API_URL || "https://api.suavus.app";
+export const API_URL = import.meta.env.PUBLIC_API_URL || "https://zyoea-app-alpha-beta.onrender.com";
 export const GRAPHQL_URL = `${API_URL}/query`;
 export const FRONTEND_URL = import.meta.env.PUBLIC_FRONTEND_URL || "https://suavus.app";
 export const STRIPE_PUBLIC_KEY = import.meta.env.PUBLIC_STRIPE_KEY || "";
