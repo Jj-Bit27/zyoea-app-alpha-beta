5b2b5933b5843d2e6d4e1c0bf22da32d6efbddb7
Juan Jose <juanglezjj070927@gmail.com>
2026-08-07 02:20:48 -0600

    Modificando la URL del frontend

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,4 +1,4 @@
 export const API_URL = import.meta.env.PUBLIC_API_URL || "https://api.suavus.app";
 export const GRAPHQL_URL = `${API_URL}/query`;
-export const FRONTEND_URL = import.meta.env.PUBLIC_FRONTEND_URL || "http://suavus.app";
+export const FRONTEND_URL = import.meta.env.PUBLIC_FRONTEND_URL || "https://suavus.app";
 export const STRIPE_PUBLIC_KEY = import.meta.env.PUBLIC_STRIPE_KEY || "";
