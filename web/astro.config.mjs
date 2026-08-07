e628080e1d767805d6b129ebb01262057f79797f
Juan Jose <juanglezjj070927@gmail.com>
2026-08-07 02:21:37 -0600

    Añadiendo la configuracion de vercel en la configuracion de astro.config.mjs

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,10 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
